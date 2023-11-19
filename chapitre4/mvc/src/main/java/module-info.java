7b91d974620008dd45ce805ba69633d42dd6c725
Gilles Poulet <gilles.poulet@gmail.com>
2023-11-19 15:31:46 +0100

    Modified module-info.java.

--- chapitre4/mvc/src/main/java/module-info.java
+++ chapitre4/mvc/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module be.iramps.ue1103.mvc {
-    requires java.desktop;
-    requires javafx.controls;
+    requires transitive java.desktop;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires org.postgresql.jdbc;
 
     opens be.iramps.ue1103.mvc to javafx.fxml;
-    exports be.iramps.ue1103.mvc.View;
+    exports be.iramps.ue1103.mvc.View;    
+    exports be.iramps.ue1103.mvc.Controller; 
+    exports be.iramps.ue1103.mvc.Model;
 }
